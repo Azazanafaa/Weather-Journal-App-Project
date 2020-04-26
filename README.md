b5f83791a87bf07167558736fdd214965e0ff18d
nazaza <azazanafaa@gmail.com>
2020-04-26 20:07:27 +0200

    @Weather app project

---
+++ README.md
@@ -0,0 +1,34 @@
+# Weather Journal App
+This project is a part of Udacity's nanodegree program for The fornt end developper track. It's a simple web page in which you can enter the city and a comment to search for the weather forecast of the city you entred. This project uses OpenWeatherMap api to get the weather and node to store the information locally. You may want to change the api key to get it work.
+
+
+## Structure
+
+The structure of the project is as below:
+```
+website
+|__ app.js
+|__ index.html
+|__ styles.css    
+package.json
+package-lock.json
+server.js
+README.md
+```
+
+## Install
+
+```
+$ git clone-the-https://github.com/Azazanafaa/Weather-Journal-App-Project.git
+$ cd Weather-Journal-App-Project/
+$ npm install/
+``` 
+
+## Contributing
+
+Feel free to modify and add your own sections. I will be ***more than happy***.
+
+## License
+The content of this repository is licensed under a [The MIT License](https://opensource.org/licenses/MIT) .
+Check the OpenWeatherMap license.
+
---
+++ website/app.js
@@ -0,0 +1,134 @@
+/* Global Variables */
+const apikey = '&APPID=ffed4aa63204dd3d8fb822260953b11e';
+const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const iconLink = 'http://openweathermap.org/img/wn/';
+const iconSuffix = '@2x.png'
+
+const cityInput = document.getElementById('city-name');
+const submitBtn = document.getElementById('submitForm');
+const errorMsg = document.querySelector('.error-msg');
+const feelingText = document.getElementById('feelings');
+
+const cardImg = document.getElementById('weather-icon');
+const cardDate = document.getElementById('date');
+const cardLocation = document.getElementById('location');
+const cardStatus = document.getElementById('status');
+const cardTemp = document.getElementById('temp');
+const cardFeelings = document.getElementById('content');
+const infoMsg = document.getElementById('info-msg');
+const weatherCard = document.getElementById('weather-card');
+const allSearchSection = document.getElementById('all-search');
+const baseUrl = 'http://localhost:8080';
+let weatherData;
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear();
+
+/**************************** Getting weather from openweathermap api */
+const getWeather = async (url = '') => {
+    const response = await fetch(url);
+    try {
+        const data = await response.json();
+        if (data.message && data.message == 'city not found') {
+            weatherData = null;
+            infoMsg.style.display = 'block';
+            weatherCard.style.display = 'none';
+            infoMsg.innerText = 'Please enter a valid city name'
+
+        } else {
+            infoMsg.style.display = 'none';
+            weatherCard.style.display = 'flex';
+            const feeling = feelingText.value ? feelingText.value : '';
+            weatherData = {
+                date: newDate,
+                location: data.name,
+                status: data.weather[0].main,
+                temp: data.main.temp + ' °F',
+                feelings: feeling,
+                icon: iconLink + data.weather[0].icon + iconSuffix
+            };
+            console.log(weatherData);
+        }
+        return weatherData;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/******************************* Saving searched data on the server */
+const saveData = async (url = '', data = {}) => {
+    const response = await fetch(baseUrl + url, {
+        method: 'POST',
+        cridentials: 'same-orgin',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+    try {
+        const serverRes = await response.body;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/*************************** Get all data from the server */
+const getAllData = async (url = '') => {
+    const response = await fetch(baseUrl + url);
+    try {
+        const allData = await response.json();
+        return allData;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/**************************************** Updating card with the new info */
+function updateWeatherCardUI(weatherData) {
+    cardImg.setAttribute('src', weatherData.icon);
+    cardDate.innerText = '';
+    cardDate.insertAdjacentHTML('beforeend', `<strong>Search Date: </strong>${weatherData.date}`);
+    cardLocation.innerText = '';
+    cardLocation.insertAdjacentHTML('beforeend', `<strong>location: </strong>${weatherData.location}`);
+    cardStatus.innerText = '';
+    cardStatus.insertAdjacentHTML('beforeend', `<strong>Status: </strong>${weatherData.status}`);
+    cardTemp.innerText = '';
+    cardTemp.insertAdjacentHTML('beforeend', `<strong>Temperature: </strong>${weatherData.temp}`);
+    cardFeelings.innerText = '';
+    cardFeelings.insertAdjacentHTML('beforeend', `<strong>Feelings: </strong>${weatherData.feelings}`);
+
+}
+
+/*********************************  Adding previous searchs to the view */
+function updateAllSearchUI(weatherData) {
+    allSearchSection.innerText = '';
+    for (let data of weatherData) {
+        let card = `<div class="serach-card">
+        <div class="info" ><strong>Date: </strong>${data.date}</div>
+        <div class="info" ><strong>location: </strong>${data.location}</div>
+        <div class="info" ><strong>Status: </strong>${data.status}</div>
+        <div class="info" ><strong>Temperature: </strong>${data.temp}</div>
+      </div>`
+      allSearchSection.insertAdjacentHTML('afterbegin', card);
+    }
+}
+
+/******************************** Adding on click event to the submit form */
+submitBtn.addEventListener('click', () => {
+    if (cityInput.value && cityInput.value != '') {
+        getWeather(apiUrl + cityInput.value + apikey).then((data) => {
+            if (data) {
+                updateWeatherCardUI(data);
+                saveData('/saveSearch', data);
+            }
+        }).then(() => {
+            return getAllData('/allSearch');
+        }).then((data) => {
+            updateAllSearchUI(data);
+        });
+        errorMsg.style.display = 'none';
+    } else {
+        errorMsg.style.display = 'block';
+    }
+});
+
+
